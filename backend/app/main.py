8c86ecfe1e49995b4516e2a66f3631d7aed570b7
Vasanthan Yogeshwaran <yvasanthan007@gmail.com>
2026-07-22 13:07:27 +0530

    feat: create dashboard API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="MentorAI API")
+from app.api.dashboard import router as dashboard_router
+
+app = FastAPI(title="MentorAI Backend")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(dashboard_router)
 
 @app.get("/")
 def home():
     return {
-        "message": "MentorAI Backend Running"
+        "message":"Backend Running"
     }
